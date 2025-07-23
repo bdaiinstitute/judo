4d5da43c06376c768516f1c4c48477d959b59750
Albert Li <alberthli@caltech.edu>
2025-07-22 17:36:07 -0700

    add monkey patch for adjusting MJ_CCD_ITERATIONS

--- judo/__init__.py
+++ judo/__init__.py
@@ -2,5 +2,11 @@
 
 from pathlib import Path
 
+from judo.utils.patch import patch_mj_ccd_iterations
+
 PACKAGE_ROOT = Path(__file__).resolve().parent
 MODEL_PATH = PACKAGE_ROOT / "models"
+
+# TEMPORARY: Patch MJ_CCD_ITERATIONS globally for the session
+# Once this PR is resolved, remove the monkey patch logic: https://github.com/google-deepmind/mujoco_warp/issues/456
+patch_mj_ccd_iterations(32)
