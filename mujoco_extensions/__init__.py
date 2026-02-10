8ad325502a78900e2688cceebae384ec143269bc
Duy Ta <dta@theaiinstitute.com>
2026-02-10 11:29:41 -0500

    remove jacobian_smoothing

--- mujoco_extensions/__init__.py
+++ mujoco_extensions/__init__.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2025 Robotics and AI Institute LLC. All rights reserved.
 
-from mujoco_extensions import jacobian_smoothing, policy_rollout
+from mujoco_extensions import policy_rollout
 
 __all__ = [
     "policy_rollout",
-    "jacobian_smoothing",
 ]
--- mujoco_extensions/jacobian_smoothing/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) 2025 Robotics and AI Institute LLC. All rights reserved.
-
-from mujoco_extensions.jacobian_smoothing.jacobian_smoothing_pybind.jacobian_smoothing import *  # noqa: F403
